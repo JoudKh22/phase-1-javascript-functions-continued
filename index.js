78884992c931b89548e5c1f27055ac40cba4587c
Joud Khanji <joud.hanji@gmail.com>
2023-07-20 22:11:58 +0300

    test

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate') {
+  return `This Saturday, I want to ${activity}!`;
+}
+console.log(saturdayFun('bathe my dog')); 
+
+
+function mondayWork(task = 'go to the office') {
+  return `This Monday, I will ${task}.`;
+}
+console.log(mondayWork('work from home')); 
+
+
+function wrapAdjective(type = '*') {
+  return function (adjective = 'special') {
+    return `You are ${type}${adjective}${type}!`;
+  };
+}
+const encouragingPromptFunction = wrapAdjective('||');
+console.log(encouragingPromptFunction('a dedicated programmer')); 
+
